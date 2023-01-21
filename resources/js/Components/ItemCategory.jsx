fbb53b7095d84a1b8afb188ae63181721842ecf4
unknown <nurfadli231298@gmail.com>
2023-01-22 00:31:40 +0800

    fix whitespace category name in catalog page

--- resources/js/Components/ItemCategory.jsx
+++ resources/js/Components/ItemCategory.jsx
@@ -3,7 +3,7 @@ export default function ItemCategory({href, title, active}){
         <li>
             <a 
                 href={href} 
-                className={`slide-category px-4 py-2 rounded-full text-sm border border-pinif-1 ${
+                className={`whitespace-nowrap slide-category px-4 py-2 rounded-full text-sm border border-pinif-1 ${
                     active 
                         ? 'bg-pinif-1 font-semibold text-white shadow-md shadow-pinif-1/50'
                         :' bg-white text-pinif-1 hover:shadow-md hover:shadow-pinif-1/50'
